3f99ff6159f24c5643273ec8d1d614de652b5076
ttchef <ruro0.edit@gmail.com>
2026-02-22 10:22:52 +0100

    removed cmake build files

--- CMakeFiles/shaders.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/shaders"
-  "sausages.arc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
